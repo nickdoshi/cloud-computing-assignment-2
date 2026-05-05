f9998f880d93fcdcecfafec2cc248ea963c1ae35
Nicholas Doshi <s3980727@student.rmit.edu.au>
2026-05-05 23:18:58 +1000

    add: config.js – BACKEND_URL switcher for EC2/ECS/Lambda

---
+++ frontend/js/config.js
@@ -0,0 +1,5 @@
+// Swap this URL to point at whichever backend you are demoing.
+// EC2:     'http://<EC2_PUBLIC_IP>'
+// ECS:     'http://<ALB_DNS_NAME>'
+// Lambda:  'https://<API_GATEWAY_ID>.execute-api.us-east-1.amazonaws.com/<stage>'
+const BACKEND_URL = 'http://localhost:8080';
